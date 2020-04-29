233b26009971c29985f0c5c1847154f58ebdc724
Yash Kulkarni <60318826+Selium98@users.noreply.github.com>
2020-04-29 15:15:24 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'retrovis_db' );
 
 /** MySQL database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
